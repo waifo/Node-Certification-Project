aea7cb8aa85e0e673e3fd240435b4d0e5b5ab5da
Mukesh Kumar <M1036022@mindtree.com>
2017-12-09 04:33:52 +0530

    Static path added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var app=express();
 
 app.set('port', (process.env.PORT || 4000));
+app.use(express.static('public'))
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
